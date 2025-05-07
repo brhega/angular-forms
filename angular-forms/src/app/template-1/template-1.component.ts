5ce835367d504d3b06289a9ce5e5df8b0d9d3796
Brock Hegarty <brock@fixation.dev>
2025-05-06 21:21:42 -0500

    register model

--- angular-forms/src/app/template-1/template-1.component.ts
+++ angular-forms/src/app/template-1/template-1.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RegistrationModel } from './RegistrationModel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-1',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './template-1.component.html',
   styleUrl: './template-1.component.scss'
 })
 export class Template1Component {
+  formModel = new RegistrationModel();
 
+  public onSubmit(): void {
+    // Your existing code here
+  }
 }

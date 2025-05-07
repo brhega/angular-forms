d1986c96f66e3a4e43111014546ad864f01936c7
Brock Hegarty <brock@fixation.dev>
2025-05-06 20:55:27 -0500

    new angular app

---
+++ angular-forms/src/app/template-1/template-1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-1',
+  imports: [],
+  templateUrl: './template-1.component.html',
+  styleUrl: './template-1.component.scss'
+})
+export class Template1Component {
+
+}

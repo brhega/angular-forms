07a7126292e4e9d9c316a71872963eb132c0c170
Brock Hegarty <brock@fixation.dev>
2025-05-07 22:35:19 -0500

    copy contents for template 3

--- angular-forms/src/app/app.routes.ts
+++ angular-forms/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Template1Component } from './template-1/template-1.component';
 import { Template2Component } from './template-2/template-2.component';
+import { Template3Component } from './template-3/template-3.component';
 
 export const routes: Routes = [
   { path: 'template-1', component: Template1Component },
   { path: 'template-2', component: Template2Component },
+  { path: 'template-3', component: Template3Component },
   { path: '', redirectTo: 'template-1', pathMatch: 'full' },
 ];
---
+++ angular-forms/src/app/template-3/template-3.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { RegistrationModel } from './RegistrationModel';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-3',
+  imports: [FormsModule],
+  templateUrl: './template-3.component.html',
+  styleUrl: './template-3.component.scss'
+})
+export class Template3Component {
+  formModel = new RegistrationModel();
+  
+  public onSubmit(form: NgForm): void {
+    if (!form.valid) {
+      form.control.markAllAsTouched();
+    }
+    else {
+      alert(`Form Submitted!\n
+        First Name: ${this.formModel.firstName}
+        Last Name: ${this.formModel.lastName}
+        Email: ${this.formModel.email}
+        Phone: ${this.formModel.phone}`);
+
+    }
+  }
+}

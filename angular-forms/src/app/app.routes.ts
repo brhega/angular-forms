32956d9582e9cfb4245f29748685d463cad6677e
Brock Hegarty <brockhegarty@msn.com>
2025-05-08 20:27:48 -0500

    reactive 1

--- angular-forms/src/app/app.routes.ts
+++ angular-forms/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Template1Component } from './template-1/template-1.component';
 import { Template2Component } from './template-2/template-2.component';
 import { Template3Component } from './template-3/template-3.component';
 import { Template4Component } from './template-4/template-4.component';
+import { Reactive1Component } from './reactive-1/reactive-1.component';
 
 export const routes: Routes = [
   { path: 'template-1', component: Template1Component },
   { path: 'template-2', component: Template2Component },
   { path: 'template-3', component: Template3Component },
   { path: 'template-4', component: Template4Component },
+  { path: 'reactive-1', component: Reactive1Component },
   { path: '', redirectTo: 'template-1', pathMatch: 'full' },
 ];
---
+++ angular-forms/src/app/reactive-1/reactive-1.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-1',
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-1.component.html',
+  styleUrls: ['./reactive-1.component.scss']
+})
+export class Reactive1Component {
+  form = new FormGroup({
+    firstName: new FormControl(null),
+    lastName: new FormControl(null),
+    email: new FormControl(null),
+    phone: new FormControl(null)
+  });
+
+  public onSubmit(): void {
+    const { firstName, lastName, email, phone } = this.form.value;
+    alert(`Form Submitted!\n      
+      First Name: ${firstName}    
+      Last Name: ${lastName}      
+      Email: ${email}     
+      Phone: ${phone}`);
+  }
+}
